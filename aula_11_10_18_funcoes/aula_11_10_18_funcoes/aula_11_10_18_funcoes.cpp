fb99848c4b739153a716d8024bb5e3cb29391ba0
rafael kohler <rafael.kohler@gmail.com>
2018-10-11 21:06:17 -0300

    aula de funções

---
+++ aula_11_10_18_funcoes/aula_11_10_18_funcoes/aula_11_10_18_funcoes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+
+void helooWorld(){
+    printf("\nHello World!\n");
+}
+
+int somar2valores(int x, int y){
+    int c;
+    c = x + y;
+    return c;
+    
+}
+
+int lerNumeroInteiro(){
+    printf("\nFavor digitar um numero inteiro: ");
+    int numero;
+    scanf("%i", &numero);
+    return numero;
+}
+
+int main2(){
+    helooWorld();
+    
+    int a = somar2valores(3, 39);
+    printf("O resultado da soma: %i\n", a);
+    
+    int b = 10;
+    int c = 15;
+    int d = somar2valores(b, c);
+    printf("Resultado da soma: %i\n", d);
+    
+    int primeiroValor = lerNumeroInteiro();
+    int segundoValor = lerNumeroInteiro();
+    int e = somar2valores(primeiroValor, segundoValor);
+    printf("Resultado da soma: %i\n", e);
+    
+    return 0;
+}
