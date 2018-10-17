04cccfc36ca6f9092e2479d18e18377cefad6eba
rafael kohler <rafael.kohler@gmail.com>
2018-10-16 22:23:20 -0300

    Acréscimo de Else.

--- 16_10_18/16_10_18/16_10_18.cpp
+++ 16_10_18/16_10_18/16_10_18.cpp
@@ -18,5 +18,20 @@ int main() {
         printf("a diferente de zero\n");
     }
     
+    int b = 5;
+    
+    if(b < a){
+        printf("b eh menor que a\n\n");
+    }
+    if (a <= b) { // a pode ser menor, pode ser igual e pode ser diferente
+        printf("A menor ou igual a B\n\n");
+    }
+    
+    if(a == b) {
+        printf("sao iguais\n\n");
+    } else {
+        printf("sao diferentes\n\n");
+    }
+    
     system("pause");
 }
