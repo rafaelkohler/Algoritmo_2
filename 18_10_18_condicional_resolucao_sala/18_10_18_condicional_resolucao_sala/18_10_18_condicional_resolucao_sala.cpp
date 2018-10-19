a195c4f36fdf6d5aed4116b6589b3299d8a586c1
rafael kohler <rafael.kohler@gmail.com>
2018-10-18 22:13:02 -0300

    controle if and else com cálculo de média e mostrar os valores maiores que a média.

--- 18_10_18_condicional_resolucao_sala/18_10_18_condicional_resolucao_sala/18_10_18_condicional_resolucao_sala.cpp
+++ 18_10_18_condicional_resolucao_sala/18_10_18_condicional_resolucao_sala/18_10_18_condicional_resolucao_sala.cpp
@@ -21,12 +21,41 @@ int maiorValorInteiro(int a, int b, int c, int d){
     return maior;
 }
 
+float media(int a, int b, int c, int d) {
+    float mediaResult = (((float)a + (float)b + (float)c + (float)d) / 4);
+    return mediaResult;
+}
+
+void maioresQueMedia (int a, int b, int c, int d, float media) {
+    printf("\nOs valores maiores que a media sao: ");
+    
+    if (a > media){
+        printf("%i ", a);
+    }
+    if (b > media){
+        printf("%i ", b);
+    }
+    if (c > media){
+        printf("%i ", c);
+    }
+    if (d >= media){
+        printf("%i ", d);
+    }
+}
+
 int main(){
     
-    int primeiro = lerNumeroInteiro();
-    int segundo = lerNumeroInteiro();
-    int terceiro = lerNumeroInteiro();
-    int quarto = lerNumeroInteiro();
+    int v1 = lerNumeroInteiro();
+    int v2 = lerNumeroInteiro();
+    int v3 = lerNumeroInteiro();
+    int v4 = lerNumeroInteiro();
+    int maior = maiorValorInteiro(v1, v2, v3, v4);
+    float mediaNum = media(v1, v2, v3, v4);
+
+    printf("\nO maior numero eh: %i", maior);
+    printf("\nA media dos numeros eh: %.2f", mediaNum);
+    maioresQueMedia(v1, v2, v3, v4, mediaNum);
+  
     
     return 0;
 }
