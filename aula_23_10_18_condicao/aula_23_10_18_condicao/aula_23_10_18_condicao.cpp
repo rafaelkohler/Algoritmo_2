ed84fe4566b21a2bab04a1380f371f2f66793eb6
rafael kohler <rafael.kohler@gmail.com>
2018-10-23 20:56:45 -0300

    Correção do código.

--- aula_23_10_18_condicao/aula_23_10_18_condicao/aula_23_10_18_condicao.cpp
+++ aula_23_10_18_condicao/aula_23_10_18_condicao/aula_23_10_18_condicao.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-float salario(){
+float lerSalario(){
     float a = 0;
     printf("Favor informar o seu salario: ");
     scanf("%f", &a);
@@ -11,16 +11,16 @@ float calcIR(double sal) {
     float resultsal = 0;
     if(sal <= 1903.98) {
         return resultsal = 0;
-    } else if (resultsal <= 2826.65){
+    } else if (sal <= 2826.65){
         resultsal = ((sal * 0.075) - 142.8);
         return resultsal;
-    } else if (resultsal <= 3751.05) {
+    } else if (sal <= 3751.05) {
         resultsal = ((sal * 0.15) - 354.8);
         return resultsal;
-    } else if (resultsal <= 4664.68) {
+    } else if (sal <= 4664.68) {
         resultsal = ((sal * 0.225) - 636.13);
         return resultsal;
-    } else (resultsal >= 4664.68);
+    } else (sal >= 4664.68);
         resultsal = ((sal * 0.275) - 869.36);
         return resultsal;
 }
@@ -28,10 +28,10 @@ float calcIR(double sal) {
 int main() {
     int resp;
     do {
-    float s = salario();
+    float s = lerSalario();
     float sal = calcIR(s);
     printf("\nO valor a ser pago de imposto eh: %.2f", sal);
-    scanf("%f", &sal);
+    printf("\nE o que soubrou foi: %.2f\n\n", s - sal);
     
     printf("\nDeseja calcular outro valor?");
     printf("\nDigite 1 para continuar ou 2 para sair.");
