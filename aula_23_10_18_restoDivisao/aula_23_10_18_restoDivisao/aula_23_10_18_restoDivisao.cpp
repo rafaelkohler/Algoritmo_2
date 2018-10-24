d35e413b175d13a8365b465fc189f2ebc068f6b1
rafael kohler <rafael.kohler@gmail.com>
2018-10-23 22:08:23 -0300

    Resolução.

--- aula_23_10_18_restoDivisao/aula_23_10_18_restoDivisao/aula_23_10_18_restoDivisao.cpp
+++ aula_23_10_18_restoDivisao/aula_23_10_18_restoDivisao/aula_23_10_18_restoDivisao.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 
 
-int main() {
-   
+
+void apresentarRestoDiviComSwitch(){
+    printf("Digite algum numero entre 0 ... 1000\n\n");
+    int numero = 0;
+    scanf("%i", &numero);
+    int resto = numero % 5;
     
-]
+    switch (resto) {
+        case 0:
+            printf("Eh divisivel");
+            break;
+        case 1:
+            printf("Sobrou 1");
+            break;
+        case 2:
+            printf("Sobrou 2");
+            break;
+        case 3:
+            printf("Sobrou 3");
+            break;
+        case 4:
+            printf("Sobrou 4");
+            break;
+            
+        default:
+            break;
+    }
+}
+
+int main() {
+    apresentarRestoDiviComSwitch();
+    
+}
