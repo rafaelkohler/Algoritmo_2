ac5818c46471e56aeec0361daa05af9789303afb
rafael kohler <rafael.kohler@gmail.com>
2018-10-30 20:56:17 -0300

    Exercício 02

--- listaDeExercicios/listaDeExercicios/exercicio_001.cpp
+++ listaDeExercicios/listaDeExercicios/exercicio_001.cpp
@@ -8,21 +8,38 @@ double lerNumeros (){
     return a;
 }
 
-double calcula (double x1, double y1, double x2, double y2) {
+double calculaEx01 (double x1, double y1, double x2, double y2) {
     double d;
     d = sqrt(pow((x2 - x1), 2) + (pow((y2 - y1), 2)));
     return d;
 }
 
+double exponencial (double x, double y) {
+    double result;
+    return result = pow((x + y), 2);
+}
+
 int main(){
     
+    printf("Exercicio_001\n");
     double x1 = lerNumeros();
     double y1 = lerNumeros();
     double x2 = lerNumeros();
     double y2 = lerNumeros();
     
-    double result = calcula(x1, y1, x2, y2);
-    printf("\nExercicio_001\n");
-    printf("Valor do resultado eh: %.2f\n\n\n", result);
+    double result = calculaEx01(x1, y1, x2, y2);
+    printf("A distancia entre os dois pontos do exercicio 01 eh: %.2f\n\n\n", result);
+    
+    printf("\nExercicio_002\n");
+    printf("Favor informar 3 numeros inteiros positivos: \n");
+    double a = lerNumeros();
+    double b = lerNumeros();
+    double c = lerNumeros();
+    
+    double r = exponencial(a, b);
+    double s = exponencial(b, c);
+    
+    double d = (r + s)/2;
+    printf("Valor do resultado do exercicio 02 eh: %.2f\n\n\n", d);
     
 }
